a8b77bb27654a200d07b5dfabcb1a1e47c3cf44f
brickstar <brickstar@gmail.com>
2019-01-13 20:43:23 -0700

    Add board initialize specs

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -9,4 +9,14 @@ describe 'board' do
   it 'exists' do
     expect(@board).to be_a(Board)
   end
+
+  it 'should initialize with a grid' do
+    expect(@board.grid).to eq(["---", "-m-", "p--"])
+  end
+
+  it 'should initialize with a bot and princess' do
+    expect(@board.pieces.length).to eq(2)
+    expect(@board.pieces.all? { |piece| piece.is_a? GamePiece }).to eq(true)
+  end
+
 end
