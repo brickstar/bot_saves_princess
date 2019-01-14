faa7fa7419c52e97aa21f43738ff1dc3c5ad783a
brickstar <brickstar@gmail.com>
2019-01-13 20:55:08 -0700

    Add private methods to Board to determine coords and place game pieces

--- lib/board.rb
+++ lib/board.rb
@@ -3,5 +3,23 @@ class Board
   def initialize(grid)
     @grid   = grid
     @pieces = []
+    place_piece('m')
+    place_piece('p')
+  end
+
+  private
+  def place_piece(letter)
+    coordinates = determine_coordinates(letter, grid)
+    @pieces << GamePiece.new(letter, coordinates[0], coordinates[1])
+  end
+
+  def determine_coordinates(letter, grid)
+    coords = []
+    grid.each do |str|
+      next if !str.include?(letter)
+      coords.push(str.index(letter))
+      coords.unshift(grid.index(str))
+    end
+    coords
   end
 end
