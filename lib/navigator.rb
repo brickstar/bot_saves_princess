0774726c837b52d86e1fd1969bb06fc0e1585108
brickstar <brickstar@gmail.com>
2019-01-13 21:34:12 -0700

    Add path to princess instance method to navigator class

--- lib/navigator.rb
+++ lib/navigator.rb
@@ -3,4 +3,33 @@ class Navigator
   def initialize(board)
     @board = board
   end
+
+  def path_to_princess
+    column_direction * column_distance.abs + row_direction * row_distance.abs
+  end
+
+  private
+  def column_distance
+    board.bot.column_position - board.princess.column_position
+  end
+
+  def row_distance
+    board.bot.row_position - board.princess.row_position
+  end
+
+  def column_direction
+    if column_distance < 0
+      "RIGHT\n"
+    else
+      "LEFT\n"
+    end
+  end
+
+  def row_direction
+    if row_distance < 0
+      "DOWN\n"
+    else
+      "UP\n"
+    end
+  end
 end
