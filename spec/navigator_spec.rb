32e48d3f4a8dfc24ed7ec302f7437b03fb3a953b
brickstar <brickstar@gmail.com>
2019-01-13 21:55:30 -0700

    Add specs for 5 and 7 sized grids, and pieces starting in the same row or column

--- spec/navigator_spec.rb
+++ spec/navigator_spec.rb
@@ -3,26 +3,62 @@ require './lib/navigator'
 
 describe 'navigator' do
   before(:each) do
-    @board1 = Board.new(["---", "-m-", "p--"])
-    @board2 = Board.new(["--p", "-m-", "---"])
-    @navigator1 = Navigator.new(@board1)
-    @navigator2 = Navigator.new(@board2)
+    @board_1 = Board.new(["---", "-m-", "p--"])
+    @board_2 = Board.new(["--p", "-m-", "---"])
+    @navigator_1 = Navigator.new(@board_1)
+    @navigator_2 = Navigator.new(@board_2)
   end
 
   context 'initializer' do
     it 'exists' do
-      expect(@navigator1).to be_a(Navigator)
+      expect(@navigator_1).to be_a(Navigator)
     end
 
     it "should initialize with a board" do
-      expect(@navigator1.board).to be_a(Board)
+      expect(@navigator_1.board).to be_a(Board)
     end
   end
 
   context '#instance methods' do
-    it '#path_to_princess' do
-      expect(@navigator1.path_to_princess).to eq("LEFT\nDOWN\n")
-      expect(@navigator2.path_to_princess).to eq("RIGHT\nUP\n")
+    context '#path_to_princess' do
+      it '3 x 3 board' do
+        expect(@navigator_1.path_to_princess).to eq("LEFT\nDOWN\n")
+        expect(@navigator_2.path_to_princess).to eq("RIGHT\nUP\n")
+      end
+
+      it '5 x 5 board' do
+        board_5 = Board.new(["--m--", "-----", "----p"])
+        board_6 = Board.new(["--p--", "-----", "----m"])
+        navigator_5 = Navigator.new(board_5)
+        navigator_6 = Navigator.new(board_6)
+
+        expect(navigator_5.path_to_princess).to eq("RIGHT\nRIGHT\nDOWN\nDOWN\n")
+        expect(navigator_6.path_to_princess).to eq("LEFT\nLEFT\nUP\nUP\n")
+      end
+
+      it '7 x 7 board' do
+        board_7 = Board.new(["--m----", "-------", "------p"])
+        board_8 = Board.new(["--p----", "-------", "------m"])
+        navigator_7 = Navigator.new(board_7)
+        navigator_8 = Navigator.new(board_8)
+
+        expect(navigator_7.path_to_princess).to eq("RIGHT\nRIGHT\nRIGHT\nRIGHT\nDOWN\nDOWN\n")
+        expect(navigator_8.path_to_princess).to eq("LEFT\nLEFT\nLEFT\nLEFT\nUP\nUP\n")
+      end
+
+      it 'pieces in same row' do
+        board = Board.new(["---", "m-p", "---"])
+        navigator = Navigator.new(board)
+
+        expect(navigator.path_to_princess).to eq("RIGHT\nRIGHT\n")
+      end
+
+      it 'pieces in same column' do
+        board = Board.new(["m--", "p--", "---"])
+        navigator = Navigator.new(board)
+
+        expect(navigator.path_to_princess).to eq("DOWN\n")
+      end
     end
   end
 end
