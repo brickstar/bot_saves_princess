e7762d7d182e4c5a285ee20a643806221476d96b
brickstar <brickstar@gmail.com>
2019-01-13 20:58:50 -0700

    Refactor test structure

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,24 +6,26 @@ describe 'board' do
     @board = Board.new(["---", "-m-", "p--"])
   end
 
-  it 'exists' do
-    expect(@board).to be_a(Board)
-  end
+  context "initializer" do
+    it 'exists' do
+      expect(@board).to be_a(Board)
+    end
 
-  it 'should initialize with a grid' do
-    expect(@board.grid).to eq(["---", "-m-", "p--"])
-  end
+    it 'should initialize with a grid' do
+      expect(@board.grid).to eq(["---", "-m-", "p--"])
+    end
 
-  it 'should initialize with a bot and princess' do
-    expect(@board.pieces.length).to eq(2)
-    expect(@board.pieces.all? { |piece| piece.is_a? GamePiece }).to eq(true)
-  end
+    it 'should initialize with a bot and princess' do
+      expect(@board.pieces.length).to eq(2)
+      expect(@board.pieces.all? { |piece| piece.is_a? GamePiece }).to eq(true)
+    end
 
-  it 'should place the pieces' do
-    expect(@board.bot.row_position).to eq(1)
-    expect(@board.bot.column_position).to eq(1)
-    expect(@board.princess.row_position).to eq(2)
-    expect(@board.princess.column_position).to eq(0)
+    it 'should place the pieces' do
+      expect(@board.bot.row_position).to eq(1)
+      expect(@board.bot.column_position).to eq(1)
+      expect(@board.princess.row_position).to eq(2)
+      expect(@board.princess.column_position).to eq(0)
+    end
   end
 
   context "#instance methods" do
