1e708d7824650e0801d14d385de8352f1943f751
brickstar <brickstar@gmail.com>
2019-01-13 20:37:17 -0700

    Add board class

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
+require './lib/board'
 
 describe 'board' do
   before(:each) do
     @board = Board.new(["---", "-m-", "p--"])
   end
-  
+
   it 'exists' do
     expect(@board).to be_a(Board)
   end
