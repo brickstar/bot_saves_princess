e41c9caa1519d05094a773e16914c7d381cac9c4
brickstar <brickstar@gmail.com>
2019-01-13 21:37:49 -0700

    Add spec to navigate another direction

--- spec/navigator_spec.rb
+++ spec/navigator_spec.rb
@@ -3,23 +3,26 @@ require './lib/navigator'
 
 describe 'navigator' do
   before(:each) do
-    @board = Board.new(["---", "-m-", "p--"])
-    @navigator = Navigator.new(@board)
+    @board1 = Board.new(["---", "-m-", "p--"])
+    @board2 = Board.new(["--p", "-m-", "---"])
+    @navigator1 = Navigator.new(@board1)
+    @navigator2 = Navigator.new(@board2)
   end
 
   context 'initializer' do
     it 'exists' do
-      expect(@navigator).to be_a(Navigator)
+      expect(@navigator1).to be_a(Navigator)
     end
 
     it "should initialize with a board" do
-      expect(@navigator.board).to be_a(Board)
+      expect(@navigator1.board).to be_a(Board)
     end
   end
 
   context '#instance methods' do
     it '#path_to_princess' do
-      expect(@navigator.path_to_princess).to eq("LEFT\nDOWN\n")
+      expect(@navigator1.path_to_princess).to eq("LEFT\nDOWN\n")
+      expect(@navigator2.path_to_princess).to eq("RIGHT\nUP\n")
     end
   end
 end
